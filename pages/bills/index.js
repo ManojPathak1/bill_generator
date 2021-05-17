fbd9f71a5bae61a974a5110cd77f8c46a45ad1ce
Manoj Pathak <manoj.pathak1215@gmail.com>
2021-05-17 21:00:00 +0530

    styles

--- pages/bills/index.js
+++ pages/bills/index.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { getFirestoreDB } from "../../utils/firestore";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import { formatDate } from "../../utils";
+import { formatDate, getAmountFromReadings } from "../../utils";
+import Paper from "@material-ui/core/Paper";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     margin: {
@@ -14,6 +25,24 @@ const useStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(1),
     },
 }));
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        "&:nth-of-type(odd)": {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
 
 const Bills = () => {
     const [bills, setBills] = useState([]);
@@ -61,6 +90,7 @@ const Bills = () => {
     };
 
     const onClickDelete = (id) => {
+      if (confirm("Do you want to delete it?")) {
         const db = getFirestoreDB();
         db.collection("paid_bills")
             .doc(id)
@@ -71,6 +101,7 @@ const Bills = () => {
             .catch((error) => {
                 console.error("Error removing document: ", error);
             });
+      }
     };
 
     const getAverage = () => {
@@ -85,27 +116,75 @@ const Bills = () => {
     };
 
     return (
-        <div>
-            <h1>{getAverage()}</h1>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            {bills.length > 0 && (<Card className={classes.root}>
+            <CardContent>
+              <Typography className={classes.pos} color="textSecondary">
+                Average Amount
+              </Typography>
+              <Typography variant="h6" component="h4">
+                &#8377; {getAverage()}
+                </Typography>
+            </CardContent>
+          </Card>)}
             <Link href="/">Go to Home</Link>
-            <ul>
-                {bills.map((bill) => {
-                    return (
-                        <div>
-                            {bill.date} - {bill.amount}{" "}
-                            <button onClick={() => onClickDelete(bill.id)}>
-                                Delete
-                            </button>
-                        </div>
-                    );
-                })}
-            </ul>
-            <div>
+            {bills.length === 0 && <CircularProgress />}
+            {bills.length > 0 && (
+                <TableContainer component={Paper} className={classes.margin}>
+                    <Table
+                        className={classes.table}
+                        aria-label="customized table"
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell>
+                                    Bill Payment Date
+                                </StyledTableCell>
+                                <StyledTableCell>Amount</StyledTableCell>
+                                <StyledTableCell>Action</StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {bills.map((row) => (
+                                <StyledTableRow key={row.afterDays}>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.date}
+                                    </StyledTableCell>
+                                    <StyledTableCell>
+                                        {row.amount}
+                                    </StyledTableCell>
+                                    <StyledTableCell>
+                                        <Button
+                                            variant="contained"
+                                            size="large"
+                                            color="secondary"
+                                            className={classes.margin}
+                                            onClick={() =>
+                                                onClickDelete(row.id)
+                                            }
+                                        >
+                                            Delete
+                                        </Button>
+                                    </StyledTableCell>
+                                </StyledTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
+            <div style={{ display: "flex", flexDirection: "column" }}>
                 <h3>Add Bill</h3>
                 <TextField
                     id="datetime-local"
                     label="Previous Reading Date"
                     type="datetime-local"
+                    className={classes.margin}
                     variant="outlined"
                     InputLabelProps={{
                         shrink: true,
@@ -121,6 +200,7 @@ const Bills = () => {
                         shrink: true,
                     }}
                     variant="outlined"
+                    className={classes.margin}
                     value={amount}
                     onChange={(event) => setAmount(event.target.value)}
                 />
@@ -131,7 +211,7 @@ const Bills = () => {
                     className={classes.margin}
                     onClick={onAddBill}
                 >
-                    Calculate Bill
+                    Add Bill
                 </Button>
             </div>
         </div>
